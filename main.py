3fdf57f175e554961ef7156efb2ee04439043751
David Yin <29613693+E137299@users.noreply.github.com>
2023-09-26 10:33:22 -0500

    Added Boxlayout, StackLayout

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
+class LayoutAssignment(BoxLayout):
+    pass
+
+class GridLayoutExample(GridLayout):
+    pass
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = "lr-tb"
-        b = Button(text="A", size_hint=(.5,.5))
+        for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
+            b = Button(text="Button "+letter, size_hint=(.2,.2
 
+                                                         ))
+            self.add_widget(b)
 class BoxLayoutExample(BoxLayout):
     pass
 
